ac91d07de02c78173cd55784993662625cad62f2
naru-alt <talktonaru@gmail.com>
2023-12-25 23:36:57 -0300

    Adicionei o arquivo com o desafio, com uma adição de opção de gênero e arquivo readme com as instruções do desafio.

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 //insira o nome do seu herói/heroína:
-let nomeHeroi = "Naru";
+let nomeHeroi = "Naru"
 
 //insira o nível do seu herói/heroína:
-let experienciaDoHeroi = 5002;
+let experienciaDoHeroi = 5002
 
 // insira o gênero do seu herói/heroína (M para masculino, F para feminino):
-let generoHeroi = "F";
+let generoHeroi = "F"
 
-let generoDoHeroi = (generoHeroi === "F") ? "A heroína" : "O herói";
+let generoDoHeroi = (generoHeroi === "F") ? "A heroína" : "O herói"
 
 
 if (experienciaDoHeroi <= 1000) {
-    nivelDoHeroi = "Ferro";
+    nivelDoHeroi = "Ferro"
 } else if (experienciaDoHeroi >= 1001 && experienciaDoHeroi <= 2000) {
-    nivelDoHeroi = "Bronze";
+    nivelDoHeroi = "Bronze"
 } else if (experienciaDoHeroi >= 2001 && experienciaDoHeroi <= 5000) {
-    nivelDoHeroi = "Prata";
+    nivelDoHeroi = "Prata"
 } else if (experienciaDoHeroi >= 5001 && experienciaDoHeroi <= 7000) {
-    nivelDoHeroi = "Ouro";
+    nivelDoHeroi = "Ouro"
 } else if (experienciaDoHeroi >= 7001 && experienciaDoHeroi <= 8000) {
-    nivelDoHeroi = "Platina";
+    nivelDoHeroi = "Platina"
 } else if (experienciaDoHeroi >= 8001 && experienciaDoHeroi <= 9000) {
-    nivelDoHeroi = "Ascendente";
+    nivelDoHeroi = "Ascendente"
 } else if (experienciaDoHeroi >= 9001 && experienciaDoHeroi <= 10000) {
-    nivelDoHeroi = "Imortal";
+    nivelDoHeroi = "Imortal"
 } else {
-    nivelDoHeroi = "Radiante";
+    nivelDoHeroi = "Radiante"
 }
 
 
